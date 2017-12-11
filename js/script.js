b666b90014bb9476ebf4fb8ca652dae8baf81693
Maged Eladawy <maged@ebi.ac.uk>
2017-12-11 15:12:48 +0000

    Advanced search for mobile and togglemenu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+$(document).foundation();
+
+$(document).ready(function () {
+  $("[data-responsive-toggle]").click(function () {
+    var targetMenuClass =  $(this).attr("data-responsive-toggle");
+
+    // Add class "open" to the target menu.
+    $("[class*='" + targetMenuClass + "']").toggleClass("open");
+  })
+});
+
 document.addEventListener('DOMContentLoaded', function() {
   var multiSelectChoices = new Choices('.choices[multiple]', {
     delimiter: ',',
