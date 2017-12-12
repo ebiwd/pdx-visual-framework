fdd826eda7dfee767c7506355633a9b37f3a399f
Maged Eladawy <maged@ebi.ac.uk>
2017-12-12 11:04:30 +0000

    Build first page with existing components

--- js/script.js
+++ js/script.js
@@ -7,17 +7,18 @@ $(document).ready(function () {
     // Add class "open" to the target menu.
     $("[class*='" + targetMenuClass + "']").toggleClass("open");
   })
-});
 
-document.addEventListener('DOMContentLoaded', function() {
-  var multiSelectChoices = new Choices('.choices[multiple]', {
-    delimiter: ',',
-    editItems: true,
-    maxItemCount: 5,
-    removeItemButton: true,
-  });
+  // Initialize Choices.
+  if($(".choices").length > 0) {
+    var multiSelectChoices = new Choices('.choices[multiple]', {
+      delimiter: ',',
+      editItems: true,
+      maxItemCount: 5,
+      removeItemButton: true,
+    });
 
-  var singleSelectChoices = new Choices('.choices:not([multiple])', {
-    removeItemButton: false,
-  });
+    var singleSelectChoices = new Choices('.choices:not([multiple])', {
+      removeItemButton: false,
+    });
+  }
 });
