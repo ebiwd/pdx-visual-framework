6ac63a94925e75a20fb2f7601225442f80f3294a
Maged Eladawy <maged@ebi.ac.uk>
2018-01-16 15:26:51 +0000

    Integrating handlebars and loading dynamic cancer types, providers and Choices options

--- js/script.js
+++ js/script.js
@@ -6,7 +6,30 @@ $(document).ready(function () {
 
     // Add class "open" to the target menu.
     $("[class*='" + targetMenuClass + "']").toggleClass("open");
-  })
+  });
+
+
+  if($("select#varient").length > 0) {
+    // Ajax Request for Varient Choices Select
+    var varientRequest = function (callback) {
+      fetch('https://gist.githubusercontent.com/meladawy/ebc4afc00f6c583437ff4cdee381f0d1/raw/45f9df2d298e0af19899884d5a00d16d126741e6/gistfile1.json')
+        .then(function (response) {
+          response.json().then(function (data) {
+            callback(data.suggestions, 'value', 'label');
+          });
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    };
+
+    var selectVarientChoices = new Choices('select#varient', {
+      removeItemButton: true,
+      delimiter: ',',
+    }); 
+
+    selectVarientChoices.ajax(varientRequest);
+  }
 
   // Initialize Choices.
   if($(".choices").length > 0) {
@@ -21,4 +44,12 @@ $(document).ready(function () {
       removeItemButton: false,
     });
   }
+
+  // When Click on View All Cancer Types 
+  $("a.view-all-cancer-types").click(function(ev) {
+    $("#cancer-types").attr("style", "height:350px; overflow:auto;");
+    $(this).hide();
+  });
+
+
 });
